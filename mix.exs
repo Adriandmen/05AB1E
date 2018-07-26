8a0a4ee4456d5a0a50f98fc1ea67f3a3d37d63c2
Adriandmen <A.D.Mensing-1@student.tudelft.nl>
2018-07-26 17:53:59 +0200

    adding code coverage to the ci

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule Osabie.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:excoveralls, "~> 0.9.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
