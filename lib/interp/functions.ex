e4144d1dd375d830432fdc77deabebe08796235f
Adriandmen <A.D.Mensing-1@student.tudelft.nl>
2018-10-17 14:06:24 +0200

    remove debug statement

--- lib/interp/functions.ex
+++ lib/interp/functions.ex
@@ -198,7 +198,7 @@ defmodule Interp.Functions do
 
     def try_default([function], exception_function), do: try_default(function, exception_function)
     def try_default([function | remaining], exception_function), do: (try do function.() rescue _ -> try_default(remaining, exception_function) end)
-    def try_default(function, exception_function), do: function.() #(try do function.() rescue x -> exception_function.(x) end)
+    def try_default(function, exception_function), do: (try do function.() rescue x -> exception_function.(x) end)
 
     defp throw_test_or_return(exception, value) do
         case Globals.get().debug.test do
