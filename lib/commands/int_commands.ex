bb62507722e68af12123cf09d9a3ad7dda4721bd
Adriandmen <adrianmensing1@gmail.com>
2020-10-16 12:05:03 +0200

    added an extra failsafe for the pow method

--- lib/commands/int_commands.ex
+++ lib/commands/int_commands.ex
@@ -80,8 +80,8 @@ defmodule Commands.IntCommands do
         end 
     end
     defp pow(_, 0, acc), do: acc
+    defp pow(n, k, acc) when k >= 0 and k < 1, do: acc * :math.pow(n, k)
     defp pow(n, k, acc) when is_float(n) or is_float(k), do: acc * :math.pow(n, k)
-    defp pow(n, k, acc) when k > 0 and k < 1, do: acc * :math.pow(n, k)
     defp pow(n, k, acc), do: pow(n, k - 1, n * acc)
 
     # Modulo operator:
